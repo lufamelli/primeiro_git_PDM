5e6cab3827dcd20a968a7b92183715cecc6ac785
Lucas Famelli <lucasfamelli@hotmail.com>
2021-02-12 21:27:15 -0300

    adcionando funcionalidade

--- entregagit.java
+++ entregagit.java
@@ -1,5 +1,8 @@
-public class Entregagit{
-  public static void main (String[] args) {
-    System.out.println("Entregando tarefas com Git na linha de comando");
+import javax.swing.JOptionPane;
+public Class Entregagit{
+  public static void main(String[] args) {
+    String nome = JOptionPane.showInputDialog("Qual o seu nome?");
+    JOptionPane.showMessageDialog(null, String.format("Bem vindo %s", nome));
+    //adcionando funcionalidade
   }
 }
